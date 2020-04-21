1f720db964fde225d728b2fdb25092e7e26f8efc
Messaoud <dzdevsoft@gmail.com>
2020-04-22 01:49:17 +0200

    implementing controllers and Dto's Travis fail IT fixed

--- src/test/resources/fake_data.sql
+++ src/test/resources/fake_data.sql
@@ -14,22 +14,22 @@ VALUES ('6', 'Marseille', 'France', '76 boulevard de paris', NULL, '13000');
 
 -- ############## FAKE INFLUENCERS ################
 INSERT INTO influencer
-VALUES ('5e94d9a0-3123-4523-9b71-4236b4cef93e', '2020-04-10 02:29:15.644', NULL, 'john@westeros.com', true,
+VALUES ('5e94d9a0-3123-4523-9b71-4236b4cef93e', '2020-04-10 02:29:15.644', NULL, 'john@westeros.com', TRUE,
         '2020-04-10 02:29:15.644', NULL, 'Sand', NULL, 'John', 'LIFESTYLE', '1', NULL);
 INSERT INTO influencer
-VALUES ('bafd0939-8501-4425-bce3-d18e304dc148', '2020-04-10 22:15:14.483', NULL, NULL, NULL,
+VALUES ('bafd0939-8501-4425-bce3-d18e304dc148', '2020-04-10 22:15:14.483', NULL, 'allo4@gmail.com', TRUE,
         '2020-04-10 22:15:14.483', NULL, 'Gonzalez', NULL, 'Maria', 'LIFESTYLE', '2', NULL);
 INSERT INTO influencer
-VALUES ('094a7fc4-0e73-499a-8c32-2ea2a1431366', '2020-04-11 00:21:37.406', NULL, NULL, NULL,
+VALUES ('094a7fc4-0e73-499a-8c32-2ea2a1431366', '2020-04-11 00:21:37.406', NULL, 'allo5@gmail.com', TRUE,
         '2020-04-11 00:21:37.406', NULL, 'Ali', NULL, 'Mohammed', 'LIFESTYLE', '3', NULL);
 -- #############  FAKE COMPAGNIES ##################
 
 INSERT INTO company
-VALUES ('094a7fc4-0e73-499a-8c32-2ea2a1431367', '2020-04-11 00:21:37.406', NULL, NULL, NULL,
-        '2020-04-11 00:21:37.406', 'Boulangerie BonPain', '1234567889231', '4');
+VALUES ('094a7fc4-0e73-499a-8c32-2ea2a1431367', '2020-04-11 00:21:37.406', NULL, 'allo@gmail.com', TRUE,
+        '2020-04-11 00:21:37.406', 'Boulangerie BonPain', '1234567889231', '4', NULL);
 INSERT INTO company
-VALUES ('094a7fc4-0e73-499a-8c32-2ea2a1431368', '2020-04-11 00:21:37.406', NULL, NULL, NULL,
-        '2020-04-11 00:21:37.406', 'Coiffure Tendance', '1234567889232', '5');
+VALUES ('094a7fc4-0e73-499a-8c32-2ea2a1431368', '2020-04-11 00:21:37.406', NULL, 'allo2@gmail.com', TRUE,
+        '2020-04-11 00:21:37.406', 'Coiffure Tendance', '1234567889232', '5', NULL);
 INSERT INTO company
-VALUES ('094a7fc4-0e73-499a-8c32-2ea2a1431369', '2020-04-11 00:21:37.406', NULL, NULL, NULL,
-        '2020-04-11 00:21:37.406', 'Café délices', '1234567889233', '6');
+VALUES ('094a7fc4-0e73-499a-8c32-2ea2a1431369', '2020-04-11 00:21:37.406', NULL, 'allo3@gmail.com', TRUE,
+        '2020-04-11 00:21:37.406', 'Café délices', '1234567889233', '6', NULL);
